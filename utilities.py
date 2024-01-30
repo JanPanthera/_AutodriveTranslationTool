099a4a3bb635e307deba5e11b21094e70d2bd79e
Jan Görl <JanPanthera@outlook.de>
2024-01-30 18:12:37 +0100

    Update utilities.py
    ~ Added some functions
    ~ Tweaked load_setting function
    ~ Renamed config-user.py to config-custom.py

--- utilities.py
+++ utilities.py
@@ -1,19 +1,19 @@
 import configparser
-from email.policy import default
 import os
 
-# Define paths for both the default and the user-specific configuration files
 DEFAULT_CONFIG_FILE_PATH = "config/config-default.ini"
-USER_CONFIG_FILE_PATH = "config/config-user.ini"
+USER_CONFIG_FILE_PATH = "config/config-custom.ini"
 
-def load_setting(section, setting, default_value=None, default_config=False):
+def load_setting(section, setting, default_value=None, use_default_config=False):
     config = configparser.ConfigParser()
-    config_path = USER_CONFIG_FILE_PATH if os.path.exists(USER_CONFIG_FILE_PATH) or not default_config else DEFAULT_CONFIG_FILE_PATH
-    config.read(config_path)
+    if not use_default_config and os.path.exists(USER_CONFIG_FILE_PATH):
+        config.read(USER_CONFIG_FILE_PATH)
+        if config.has_section(section) and config.has_option(section, setting):
+            return config.get(section, setting)
+    config.read(DEFAULT_CONFIG_FILE_PATH)
     if config.has_section(section) and config.has_option(section, setting):
         return config.get(section, setting)
-    else:
-        return default_value
+    return default_value
 
 def save_setting(section, setting, value):
     config = configparser.ConfigParser()
@@ -27,5 +27,22 @@ def save_setting(section, setting, value):
 def reset_settings():
     config = configparser.ConfigParser()
     config.read(DEFAULT_CONFIG_FILE_PATH)
-    with open(USER_CONFIG_FILE_PATH, 'w') as user_config_file:
+    with open(USER_CONFIG_FILE_PATH, "w") as user_config_file:
         config.write(user_config_file)
+
+def reset_setting(section, setting):
+    save_setting(section, setting, load_setting(section, setting, use_default_config=True))
+
+def save_window_geometry(window):
+    save_setting("WindowGeometry", "width", str(window.winfo_width()))
+    save_setting("WindowGeometry", "height", str(window.winfo_height()))
+    save_setting("WindowGeometry", "pos_x", str(window.winfo_x()))
+    save_setting("WindowGeometry", "pos_y", str(window.winfo_y()))
+
+def load_window_geometry(window):
+    width = load_setting("WindowGeometry", "width", default_value="1366")
+    height = load_setting("WindowGeometry", "height", default_value="768")
+    pos_x = load_setting("WindowGeometry", "pos_x", default_value="100")
+    pos_y = load_setting("WindowGeometry", "pos_y", default_value="100")
+
+    window.geometry(f"{width}x{height}+{pos_x}+{pos_y}")
