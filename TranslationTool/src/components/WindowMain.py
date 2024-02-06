99afd92b38f41fc5cbd695ea6340b21498a9fb89
Jan Görl <JanPanthera@outlook.de>
2024-02-06 19:40:30 +0100

    The most significant changes in the code revolve around the renaming of the variable `config_manager` to `cfg_manager` and the subsequent refactoring of various methods to use this new variable name. Additionally, several methods were refactored to improve efficiency and readability, such as the `load_settings` method which now uses helper functions, and the `refresh_appearance` method which now checks each condition individually.
    Here is a summary of the changes:
    
    1. The variable `config_manager` was renamed to `cfg_manager` for brevity.
    2. The `load_settings` method was refactored to use helper functions `load` and `join`.
    3. The `init_ui_components` method was refactored to remove unnecessary line breaks.
    4. The `refresh_appearance` method was refactored to check each condition individually and perform the corresponding operation if true.
    5. The `_refresh_ui` method now calls `refresh_ui` instead of `refresh_ui_localization` on `options_frame`.
    6. The `_refresh_gui_theme`, `_refresh_dpi_scaling`, `_refresh_ui_localization`, `get_window_size`, `set_window_size`, `set_window_position`, `reset_window_position`, `save_window_size`, `save_window_position`, `load_window_position`, and `load_window_size` methods were all refactored to use `cfg_manager` instead of `config_manager`.
    7. The `on_closing` method was refactored to use `cfg_manager` instead of `config_manager`.
    8. The `_refresh_ui_localization` method was also updated with a loop to update the names of the tabs in the tab view.

--- TranslationTool/src/components/WindowMain.py
+++ TranslationTool/src/components/WindowMain.py
@@ -9,19 +9,28 @@ from src.components.LanguagesFrame import LanguagesFrame
 from src.components.DictionaryFrame import DictionaryFrame
 from src.components.OptionsFrame import OptionsFrame
 
+
 class WindowMain(ctk.CTk):
     def __init__(self, parent, logger):
         super().__init__()
         self.logger = logger
         self.translation_tool = parent
 
-        self.config_manager = ConfigManager(self.logger)
+        self.cfg_manager = ConfigManager(self.logger)
 
         self.base_path = "TranslationTool/" if 'VSAPPIDDIR' in os.environ else ""
 
         self.load_settings()
         self.init_fonts()
         self.init_ui_components()
+
+        self.original_tab_names = [
+            "Translation",
+            "Languages",
+            "Dictionary",
+            "Options"
+        ]
+        self.main_tab_names = self.original_tab_names.copy()
 
         self.refresh_appearance(
             refresh_gui_theme=True,
@@ -34,20 +43,22 @@ class WindowMain(ctk.CTk):
         self.protocol("WM_DELETE_WINDOW", self.on_closing)
 
     def load_settings(self):
-        self.config_manager.add_variables({
-            "save_window_size": ctk.BooleanVar(self, value=self.config_manager.get("SaveOnWindowClose", "save_window_size", default_value="True")),
-            "save_window_pos": ctk.BooleanVar(self, value=self.config_manager.get("SaveOnWindowClose", "save_window_pos", default_value="True")),
-            "save_selected_languages": ctk.BooleanVar(self, value=self.config_manager.get("SaveOnWindowClose", "save_selected_languages", default_value="False")),
-            "use_high_dpi_scaling": ctk.BooleanVar(self, value=self.config_manager.get("Settings", "use_high_dpi_scaling", default_value="True")),
-            "ui_theme_code": ctk.StringVar(self, value=self.config_manager.get("Settings", "ui_theme", default_value="System")),
-            "ui_theme_text": ctk.StringVar(self, value=self.config_manager.get("Settings", "ui_theme", default_value="System")),
-            "ui_language_code": ctk.StringVar(self, value=self.config_manager.get("Settings", "ui_language", default_value="en")),
-            "ui_language_text": ctk.StringVar(self, value=self.config_manager.get("Settings", "ui_language", default_value="English")),
-            "selected_languages": self.config_manager.get("Settings", "selected_languages", default_value="English").split(","),
-            "supported_languages": self.config_manager.get("Settings", "supported_languages", default_value="English").split(","),
-            "input_path": os.path.join(self.base_path, self.config_manager.get("Settings", "input_path", default_value="_input")),
-            "output_path": os.path.join(self.base_path, self.config_manager.get("Settings", "output_path", default_value="_output")),
-            "dictionaries_path": os.path.join(self.base_path, self.config_manager.get("Settings", "dictionaries_path", default_value="_dictionaries"))
+        load = self.cfg_manager.load_setting
+        join = os.path.join
+        self.cfg_manager.add_vars({
+            "save_window_size": ctk.BooleanVar(self, value=load("SaveOnWindowClose", "save_window_size", default_value="True")),
+            "save_window_pos": ctk.BooleanVar(self, value=load("SaveOnWindowClose", "save_window_pos", default_value="True")),
+            "save_selected_languages": ctk.BooleanVar(self, value=load("SaveOnWindowClose", "save_selected_languages", default_value="False")),
+            "use_high_dpi_scaling": ctk.BooleanVar(self, value=load("Settings", "use_high_dpi_scaling", default_value="True")),
+            "ui_theme_code": ctk.StringVar(self, value=load("Settings", "ui_theme", default_value="System")),
+            "ui_theme_text": ctk.StringVar(self, value=load("Settings", "ui_theme", default_value="System")),
+            "ui_language_code": ctk.StringVar(self, value=load("Settings", "ui_language", default_value="English")),
+            "ui_language_text": ctk.StringVar(self, value=_(load("Settings", "ui_language", default_value="English"))),
+            "selected_languages": load("Settings", "selected_languages", default_value="English").split(","),
+            "supported_languages": load("Settings", "supported_languages", default_value="English").split(","),
+            "input_path": join(self.base_path, load("Settings", "input_path", default_value="_input")),
+            "output_path": join(self.base_path, load("Settings", "output_path", default_value="_output")),
+            "dictionaries_path": join(self.base_path, load("Settings", "dictionaries_path", default_value="_dictionaries"))
         })
 
     def init_fonts(self):
@@ -64,24 +75,19 @@ class WindowMain(ctk.CTk):
     def init_ui_components(self):
         self.title("AutoDrive Translation Tool")
 
-        self.tab_view = ctk.CTkTabview(
-            self, fg_color="transparent", bg_color="transparent")
+        self.tab_view = ctk.CTkTabview(self, fg_color="transparent", bg_color="transparent")
         self.tab_view.pack(fill="both", expand=True)
 
-        self.translation_frame = TranslationFrame(
-            self.tab_view.add(_("Translation")), self)
+        self.translation_frame = TranslationFrame(self.tab_view.add(_("Translation")), self)
         self.translation_frame.create_widgets()
 
-        self.languages_frame = LanguagesFrame(
-            self.tab_view.add(_("Languages")), self)
+        self.languages_frame = LanguagesFrame(self.tab_view.add(_("Languages")), self)
         self.languages_frame.create_widgets()
 
-        self.dictionary_frame = DictionaryFrame(
-            self.tab_view.add(_("Dictionary")), self)
+        self.dictionary_frame = DictionaryFrame(self.tab_view.add(_("Dictionary")), self)
         self.dictionary_frame.create_widgets()
 
-        self.options_frame = OptionsFrame(
-            self.tab_view.add(_("Options")), self)
+        self.options_frame = OptionsFrame(self.tab_view.add(_("Options")), self)
         self.options_frame.create_widgets()
 
     def refresh_appearance(
@@ -93,19 +99,17 @@ class WindowMain(ctk.CTk):
             self.withdraw()
             self.update_idletasks()
 
-            operations = [
-                (refresh_window_position, self.reset_window_position),
-                (refresh_window_size, self.reset_window_size),
-                (refresh_gui_theme, self._refresh_gui_theme),
-                (refresh_dpi_scaling, self._refresh_dpi_scaling),
-                (refresh_ui_localization, self._refresh_ui_localization),
-                (refresh_ui_localization, self._refresh_ui)
-            ]
-
-            for condition, operation in operations:
-                if condition:
-                    operation()
-                    self.update_idletasks()
+            if refresh_window_position:
+                self.reset_window_position()
+            if refresh_window_size:
+                self.reset_window_size()
+            if refresh_gui_theme:
+                self._refresh_gui_theme()
+            if refresh_dpi_scaling:
+                self._refresh_dpi_scaling()
+            if refresh_ui_localization:
+                self._refresh_ui_localization()
+                self._refresh_ui()
 
             self.after(100, self.deiconify)
         except Exception as e:
@@ -117,23 +121,28 @@ class WindowMain(ctk.CTk):
             self.translation_frame.refresh_ui()
             self.languages_frame.refresh_ui()
             self.dictionary_frame.refresh_ui()
-            self.options_frame.refresh_ui_localization()
+            self.options_frame.refresh_ui()
         except Exception as e:
             self.logger.error(f"Error refreshing UI: {e}")
             trigger_debug_break()
 
     def _refresh_gui_theme(self):
-        ctk.set_appearance_mode(
-            self.config_manager.get_var("ui_theme_code").get())
+        ctk.set_appearance_mode(self.cfg_manager.get_var("ui_theme_code").get())
 
     def _refresh_dpi_scaling(self):
-        if self.config_manager.get_var("use_high_dpi_scaling").get():
+        if self.cfg_manager.get_var("use_high_dpi_scaling").get():
             ctk.activate_automatic_dpi_awareness()
         else:
             ctk.deactivate_automatic_dpi_awareness()
 
     def _refresh_ui_localization(self):
-        self.translation_tool.setup_localization(language=self.config_manager.get_var("ui_language_code").get())
+        self.translation_tool.setup_localization(language=self.cfg_manager.get_var("ui_language_code").get())
+
+        for i, original_name in enumerate(self.original_tab_names):
+            translated_name = _(original_name)
+            if translated_name != self.main_tab_names[i]:
+                self.tab_view.rename(self.main_tab_names[i], translated_name)
+                self.main_tab_names[i] = translated_name
 
     def get_window_position(self):
         return handle_exception(
@@ -147,8 +156,7 @@ class WindowMain(ctk.CTk):
     def get_window_size(self):
         return handle_exception(
             self,
-            operation=lambda: tuple(
-                map(int, self.geometry().split('+')[0].split('x'))),
+            operation=lambda: tuple(map(int, self.geometry().split('+')[0].split('x'))),
             error_message="Failed to get window size",
             exception_return_value=(1366, 768),
             logger=self.logger
@@ -157,8 +165,7 @@ class WindowMain(ctk.CTk):
     def set_window_size(self, width, height):
         handle_exception(
             self,
-            operation=lambda: self.geometry(
-                f"{width}x{height}+{self.get_window_position()[0]}+{self.get_window_position()[1]}"),
+            operation=lambda: self.geometry(f"{width}x{height}+{self.get_window_position()[0]}+{self.get_window_position()[1]}"),
             error_message="Failed to set window size",
             logger=self.logger
         )
@@ -166,8 +173,7 @@ class WindowMain(ctk.CTk):
     def set_window_position(self, pos_x, pos_y):
         handle_exception(
             self,
-            operation=lambda: self.geometry(f"{self.get_window_size()[0]}x{
-                                            self.get_window_size()[1]}+{pos_x}+{pos_y}"),
+            operation=lambda: self.geometry(f"{self.get_window_size()[0]}x{self.get_window_size()[1]}+{pos_x}+{pos_y}"),
             error_message="Failed to set window position",
             logger=self.logger
         )
@@ -183,8 +189,7 @@ class WindowMain(ctk.CTk):
     def reset_window_position(self):
         handle_exception(
             self,
-            operation=lambda: self.set_window_position(
-                *self.load_window_position()),
+            operation=lambda: self.set_window_position(*self.load_window_position()),
             error_message="Failed to reset window position",
             logger=self.logger
         )
@@ -192,7 +197,7 @@ class WindowMain(ctk.CTk):
     def save_window_size(self):
         handle_exception(
             self,
-            operation=lambda: self.config_manager.save_settings([
+            operation=lambda: self.cfg_manager.save_settings([
                 ["WindowGeometry", "width", str(self.get_window_size()[0])],
                 ["WindowGeometry", "height", str(self.get_window_size()[1])]
             ]),
@@ -203,9 +208,8 @@ class WindowMain(ctk.CTk):
     def save_window_position(self):
         handle_exception(
             self,
-            operation=lambda: self.config_manager.save_settings([
-                ["WindowGeometry", "pos_x", str(
-                    self.get_window_position()[0])],
+            operation=lambda: self.cfg_manager.save_settings([
+                ["WindowGeometry", "pos_x", str(self.get_window_position()[0])],
                 ["WindowGeometry", "pos_y", str(self.get_window_position()[1])]
             ]),
             error_message="Failed to save window position",
@@ -215,11 +219,9 @@ class WindowMain(ctk.CTk):
     def load_window_position(self):
         return handle_exception(
             self,
-            operation=lambda:  (
-                int(self.config_manager.get(
-                    "WindowGeometry", "pos_x", default_value="100")),
-                int(self.config_manager.get(
-                    "WindowGeometry", "pos_y", default_value="100"))
+            operation=lambda: (
+                int(self.cfg_manager.load_setting("WindowGeometry", "pos_x", default_value="100")),
+                int(self.cfg_manager.load_setting("WindowGeometry", "pos_y", default_value="100"))
             ),
             error_message="Failed to load window position",
             exception_return_value=(100, 100),
@@ -230,10 +232,8 @@ class WindowMain(ctk.CTk):
         return handle_exception(
             self,
             operation=lambda: (
-                int(self.config_manager.get(
-                    "WindowGeometry", "width", default_value="1366")),
-                int(self.config_manager.get(
-                    "WindowGeometry", "height", default_value="768"))
+                int(self.cfg_manager.load_setting("WindowGeometry", "width", default_value="1366")),
+                int(self.cfg_manager.load_setting("WindowGeometry", "height", default_value="768"))
             ),
             error_message="Failed to load window size",
             exception_return_value=(1366, 768),
@@ -242,13 +242,12 @@ class WindowMain(ctk.CTk):
 
     def on_closing(self):
         try:
-            if self.config_manager.get_var("save_window_size").get():
+            if self.cfg_manager.get_var("save_window_size").get():
                 self.save_window_size()
-            if self.config_manager.get_var("save_window_pos").get():
+            if self.cfg_manager.get_var("save_window_pos").get():
                 self.save_window_position()
-            if self.config_manager.get_var("save_selected_languages").get():
-                self.config_manager.save("Settings", "selected_languages", ",".join(
-                    self.translation_frame.languages_to_translate.get_checked_entries()))
+            if self.cfg_manager.get_var("save_selected_languages").get():
+                self.cfg_manager.save_setting("Settings", "selected_languages", ",".join(self.translation_frame.languages_to_translate.get_checked_entries()))
 
             self.destroy()
         except Exception as e:
