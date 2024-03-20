9ac6ec5c2ac8b0ce0545a1590e109827149b54df
Jan Görl <JanPanthera@outlook.de>
2024-03-20 21:28:22 +0100

    "Added new constants to `constants.py`"
    Modified `constants.py` in `AutoDriveTranslationTool/src/core/` directory. Added new constants `APP_NAME`, `CONFIG_NAME`, `LOGGER_NAME`, `LOG_NAME`, `UI_THEMES`, `UI_COLOR_THEMES`, and `UI_LANGUAGES`. The `APP_NAME` is set to "ADT_Tool", `CONFIG_NAME` and `LOGGER_NAME` are set to the value of `APP_NAME`, `LOG_NAME` is set to the lowercase value of `APP_NAME`. `UI_THEMES` includes "light", "dark", "system". `UI_COLOR_THEMES` includes "blue", "dark-blue", "green", "ad-green". `UI_LANGUAGES` includes "english", "german", "french", "italian", "russian".

---
+++ AutoDriveTranslationTool/src/core/constants.py
@@ -0,0 +1,12 @@
+# AutoDriveTranslationTool/src/core/constants.py
+
+
+APP_NAME = "ADT_Tool"
+
+CONFIG_NAME = APP_NAME
+LOGGER_NAME = APP_NAME
+LOG_NAME = APP_NAME.lower()
+
+UI_THEMES = ["light", "dark", "system"]
+UI_COLOR_THEMES = ["blue", "dark-blue", "green", "ad-green"]
+UI_LANGUAGES = ["english", "german", "french", "italian", "russian"]
