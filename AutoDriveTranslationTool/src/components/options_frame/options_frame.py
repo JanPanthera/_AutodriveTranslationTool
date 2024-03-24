6e174a2f1a4d83e5f60d4088b5172f5649aab820
Jan Görl <JanPanthera@outlook.de>
2024-03-24 22:21:03 +0100

    Refactor options frame and logic, enhance localization
    This commit includes a significant refactor of the `options_frame.py`, `options_frame_gui.py`, and `options_frame_logic.py` files. The import statements have been updated to include new classes and remove unused ones. The `setup_callbacks` method has been renamed to `_setup_callbacks` and its implementation has been updated to use a widget-command map. The `on_language_updated` method has been updated to call a method in the logic instance when the language is updated.
    
    The `create_gui`, `create_window_settings_frame`, `create_ui_appearance_frame`, and `create_translation_settings_frame` methods in `options_frame_gui.py` have been updated to use new methods and classes for grid configuration and widget creation. New `_construct_frame` and `_configure_grid` methods have been added to `options_frame_gui.py`.
    
    The `OptionsFrameLogic` class has been updated to remove the `EventManager` import and the `_setup_event_handlers` method, suggesting a refactoring of the event-driven architecture. The scoping and management of `ctk.BooleanVar` and `ctk.StringVar` instances and the window instance have been updated. A new `_on_language_updated` method has been added to update the localization for various components of the options frame.

--- AutoDriveTranslationTool/src/components/options_frame/options_frame.py
+++ AutoDriveTranslationTool/src/components/options_frame/options_frame.py
@@ -3,18 +3,6 @@
 from .options_frame_gui import OptionsFrameGui
 from .options_frame_logic import OptionsFrameLogic
 
-from AutoDriveTranslationTool.src.core.constants import (
-    EVENT_ON_SAVE_WINDOW_SIZE_CHECKBOX, EVENT_ON_SAVE_WINDOW_POS_CHECKBOX,
-    EVENT_ON_CENTER_WINDOW_ON_STARTUP_CHECKBOX, EVENT_ON_RESET_WINDOW_SETTINGS_BUTTON,
-    EVENT_ON_RESET_WINDOW_SIZE_BUTTON, EVENT_ON_RESET_WINDOW_POS_BUTTON,
-    EVENT_ON_USE_HIGH_DPI_SCALING_CHECKBOX, EVENT_ON_UI_THEME_DROPDOWN,
-    EVENT_ON_UI_COLOR_THEME_DROPDOWN, EVENT_ON_UI_LANGUAGE_DROPDOWN,
-    EVENT_ON_RESET_UI_APPEARANCE_SETTINGS_BUTTON, EVENT_ON_WHOLE_WORD_REPLACEMENT_CHECKBOX,
-    EVENT_ON_RESET_EVERYTHING_BUTTON
-)
-from GuiFramework.utilities import EventManager
-
-
 class OptionsFrame:
     """Initialize options frame components."""
 
@@ -28,30 +16,34 @@ class OptionsFrame:
         self.loc = self.localization_manager.localize
         self.localization_manager.subscribe(self, ["lang_update"])
 
-        self.setup_callbacks()
+        self._setup_callbacks()
 
-    def setup_callbacks(self) -> None:
-        """Configure callbacks for options frame widgets."""
-        event_widget_map = {
-            EVENT_ON_SAVE_WINDOW_SIZE_CHECKBOX: self.gui_instance.checkbox_save_window_size,
-            EVENT_ON_SAVE_WINDOW_POS_CHECKBOX: self.gui_instance.checkbox_save_window_pos,
-            EVENT_ON_CENTER_WINDOW_ON_STARTUP_CHECKBOX: self.gui_instance.checkbox_center_window_on_startup,
-            EVENT_ON_RESET_WINDOW_SETTINGS_BUTTON: self.gui_instance.btn_reset_window_settings,
-            EVENT_ON_RESET_WINDOW_SIZE_BUTTON: self.gui_instance.btn_reset_window_size,
-            EVENT_ON_RESET_WINDOW_POS_BUTTON: self.gui_instance.btn_reset_window_position,
-            EVENT_ON_USE_HIGH_DPI_SCALING_CHECKBOX: self.gui_instance.checkbox_use_high_dpi_scaling,
-            EVENT_ON_UI_THEME_DROPDOWN: self.gui_instance.dropdown_ui_theme,
-            EVENT_ON_UI_COLOR_THEME_DROPDOWN: self.gui_instance.dropdown_ui_color_theme,
-            EVENT_ON_UI_LANGUAGE_DROPDOWN: self.gui_instance.dropdown_ui_language,
-            EVENT_ON_RESET_UI_APPEARANCE_SETTINGS_BUTTON: self.gui_instance.btn_reset_ui_appearance_settings,
-            EVENT_ON_WHOLE_WORD_REPLACEMENT_CHECKBOX: self.gui_instance.checkbox_whole_word_replacement,
-            EVENT_ON_RESET_EVERYTHING_BUTTON: self.gui_instance.btn_reset_everything,
+    def _setup_callbacks(self) -> None:
+        """Configure callbacks for translation frame widgets."""
+
+        widget_command_map = {
+            self.gui_instance.checkbox_save_window_size: self.logic_instance._on_save_window_size_checkbox,
+            self.gui_instance.checkbox_save_window_pos: self.logic_instance._on_save_window_pos_checkbox,
+            self.gui_instance.checkbox_center_window_on_startup: self.logic_instance._on_center_window_on_startup_checkbox,
+            self.gui_instance.btn_reset_window_settings: self.logic_instance._on_reset_window_settings_button,
+            self.gui_instance.btn_reset_window_size: self.logic_instance._on_reset_window_size_button,
+            self.gui_instance.btn_reset_window_pos: self.logic_instance._on_reset_window_pos_button,
+
+            self.gui_instance.checkbox_use_high_dpi_scaling: self.logic_instance._on_use_high_dpi_scaling_checkbox,
+            self.gui_instance.dropdown_ui_theme: self.logic_instance._on_ui_theme_dropdown,
+            self.gui_instance.dropdown_ui_color_theme: self.logic_instance._on_ui_color_theme_dropdown,
+            self.gui_instance.dropdown_ui_language: self.logic_instance._on_ui_language_dropdown,
+            self.gui_instance.btn_reset_ui_appearance_settings: self.logic_instance._on_reset_ui_appearance_settings_button,
+
+            self.gui_instance.checkbox_whole_word_replacement: self.logic_instance._on_whole_word_replacement_checkbox,
+
+            self.gui_instance.btn_reset_everything: self.logic_instance._on_reset_everything_button,
         }
 
-        for event, widget in event_widget_map.items():
-            widget.configure(command=lambda event=event: EventManager.notify(event))
+        for widget, callback in widget_command_map.items():
+            widget.configure(command=callback)
 
     def on_language_updated(self, language_code: str, event_type: str) -> None:
-        """Handle language updates for the options frame."""
+        """Handle language updates."""
         if event_type == "lang_update":
-            pass  # TODO: Implement language update for options frame
+            self.logic_instance._on_language_updated()
--- AutoDriveTranslationTool/src/components/options_frame/options_frame_gui.py
+++ AutoDriveTranslationTool/src/components/options_frame/options_frame_gui.py
@@ -2,7 +2,10 @@
 
 import customtkinter as ctk
 
-from GuiFramework.widgets.custom_tooltip import CustomTooltip
+from typing import Optional
+
+from GuiFramework.widgets import CustomCTKLabel, CustomCTKButton, CustomCTKCheckbox, CustomCTKOptionMenu
+
 from GuiFramework.utilities.config import ConfigHandler as CH
 from GuiFramework.utilities.config.config_types import ConfigKeyList as CKL
 
@@ -25,159 +28,171 @@ class OptionsFrameGui(ctk.CTkFrame):
     def create_gui(self) -> None:
         """Create GUI components for options frame."""
         self.grid(row=0, column=0, sticky="nsew")
+        self._configure_grid(self, row_weights=[(0, 0), (1, 0), (2, 1), (3, 0)], column_weights=[(0, 1), (1, 1)])
 
-        self.rowconfigure(0, weight=0)
-        self.rowconfigure(1, weight=0)
-        self.rowconfigure(2, weight=1)
-        self.rowconfigure(3, weight=0)
-        self.columnconfigure(0, weight=1)
-        self.columnconfigure(1, weight=1)
+        self.window_settings_frame = self._construct_frame(self, row=0, column=0, padx=(10, 5), pady=(10, 5), sticky="nsew")
+        self.ui_appearance_frame = self._construct_frame(self, row=0, column=1, padx=(5, 10), pady=(10, 5), sticky="nsew")
+        self.translation_settings_frame = self._construct_frame(self, row=1, column=0, padx=(10, 5), pady=(5, 10), sticky="nsew")
 
         self.create_window_settings_frame()
         self.create_ui_appearance_frame()
         self.create_translation_settings_frame()
 
-        self.btn_reset_everything = ctk.CTkButton(self, text=self.loc("btn_reset_everything"))
-        self.btn_reset_everything.configure(font=FONT_BIG_BOLD)
-        self.btn_reset_everything.grid(row=3, column=0, columnspan=2, padx=(20, 20), pady=(5, 20), sticky="nsew")
+        self.btn_reset_everything = CustomCTKButton(
+            btn_text="of_btn_reset_everything", btn_properties={"master": self, "font": FONT_BIG_BOLD},
+            pack_type="grid", pack_properties={"row": 3, "column": 0, "columnspan": 2, "padx": (20, 20), "pady": (5, 20), "sticky": "nsew"},
+            tooltip_text="of_btn_reset_everything_tt",
+            loc_func=self.localization_manager.localize
+        )
 
     def create_window_settings_frame(self) -> None:
         """Create the frame and buttons for window settings."""
-        self.window_settings_frame = ctk.CTkFrame(self)
-        self.window_settings_frame.grid(row=0, column=0, padx=(10, 5), pady=(10, 5), sticky="nsew")
+        self._configure_grid(self.window_settings_frame, row_weights=[(0, 0)], column_weights=[(0, 0)])
 
-        self.window_settings_frame.rowconfigure(0, weight=0)
-        self.window_settings_frame.columnconfigure(0, weight=0)
-
-        self.label_window_settings = ctk.CTkLabel(self.window_settings_frame, text=self.loc("label_window_settings"))
-        self.label_window_settings.configure(font=FONT_BIG_BOLD)
-        self.label_window_settings.grid(row=0, column=0, padx=(10, 10), pady=(10, 5), sticky="nsew")
-
-        self.checkbox_save_window_size = ctk.CTkCheckBox(
-            master=self.window_settings_frame,
-            text=self.loc("checkbox_save_window_size"),
-            variable=CH.get_variable_value(CKL.SAVE_WINDOW_SIZE),
-            onvalue=True,
-            offvalue=False
+        self.lbl_window_settings = CustomCTKLabel(
+            label_text="of_lbl_window_settings", label_properties={"master": self.window_settings_frame, "font": FONT_BIG_BOLD},
+            pack_type="grid", pack_properties={"row": 0, "column": 0, "padx": (10, 10), "pady": (10, 5), "sticky": "nsew"},
+            loc_func=self.localization_manager.localize
         )
-        self.checkbox_save_window_size.grid(row=1, column=0, padx=(10, 10), pady=(5, 5), sticky="nsew")
-        CustomTooltip(self.checkbox_save_window_size, self.loc("tooltip_save_window_size"))
 
-        self.checkbox_save_window_pos = ctk.CTkCheckBox(
-            master=self.window_settings_frame,
-            text=self.loc("checkbox_save_window_pos"),
-            variable=CH.get_variable_value(CKL.SAVE_WINDOW_POS),
-            onvalue=True,
-            offvalue=False
+        self.checkbox_save_window_size = CustomCTKCheckbox(
+            checkbox_text="of_checkbox_save_window_size",
+            checkbox_properties={"master": self.window_settings_frame, "variable": CH.get_variable_value(CKL.SAVE_WINDOW_SIZE), "onvalue": True, "offvalue": False, "font": FONT_BIG},
+            pack_type="grid", pack_properties={"row": 1, "column": 0, "padx": (10, 10), "pady": (5, 5), "sticky": "nsew"},
+            tooltip_text="of_checkbox_save_window_size_tt",
+            loc_func=self.localization_manager.localize
         )
-        self.checkbox_save_window_pos.grid(row=2, column=0, padx=(10, 10), pady=(5, 5), sticky="nsew")
-        CustomTooltip(self.checkbox_save_window_pos, self.loc("tooltip_save_window_pos"))
 
-        self.checkbox_center_window_on_startup = ctk.CTkCheckBox(
-            master=self.window_settings_frame,
-            text=self.loc("checkbox_center_window_on_startup"),
-            variable=CH.get_variable_value(CKL.CENTER_WINDOW_ON_STARTUP),
-            onvalue=True,
-            offvalue=False
+        self.checkbox_save_window_pos = CustomCTKCheckbox(
+            checkbox_text="of_checkbox_save_window_pos",
+            checkbox_properties={"master": self.window_settings_frame, "variable": CH.get_variable_value(CKL.SAVE_WINDOW_POS), "onvalue": True, "offvalue": False, "font": FONT_BIG},
+            pack_type="grid", pack_properties={"row": 2, "column": 0, "padx": (10, 10), "pady": (5, 5), "sticky": "nsew"},
+            tooltip_text="of_checkbox_save_window_pos_tt",
+            loc_func=self.localization_manager.localize
         )
-        self.checkbox_center_window_on_startup.grid(row=3, column=0, padx=(10, 10), pady=(5, 5), sticky="nsew")
-        CustomTooltip(self.checkbox_center_window_on_startup, self.loc("tooltip_center_window_on_startup"))
 
-        self.btn_reset_window_settings = ctk.CTkButton(self.window_settings_frame, text=self.loc("btn_reset_window_settings"))
-        self.btn_reset_window_settings.configure(font=FONT_BIG_BOLD)
-        self.btn_reset_window_settings.grid(row=4, column=0, padx=(10, 10), pady=(5, 5), sticky="nsew")
+        self.checkbox_center_window_on_startup = CustomCTKCheckbox(
+            checkbox_text="of_checkbox_center_window",
+            checkbox_properties={"master": self.window_settings_frame, "variable": CH.get_variable_value(CKL.CENTER_WINDOW_ON_STARTUP), "onvalue": True, "offvalue": False, "font": FONT_BIG},
+            pack_type="grid", pack_properties={"row": 3, "column": 0, "padx": (10, 10), "pady": (5, 5), "sticky": "nsew"},
+            tooltip_text="of_checkbox_center_window_tt",
+            loc_func=self.localization_manager.localize
+        )
 
-        self.btn_reset_window_size = ctk.CTkButton(self.window_settings_frame, text=self.loc("btn_reset_window_size"))
-        self.btn_reset_window_size.configure(font=FONT_BIG_BOLD)
-        self.btn_reset_window_size.grid(row=5, column=0, padx=(10, 10), pady=(5, 5), sticky="nsew")
+        self.btn_reset_window_settings = CustomCTKButton(
+            btn_text="of_btn_reset_window_settings", btn_properties={"master": self.window_settings_frame, "font": FONT_BIG_BOLD},
+            pack_type="grid", pack_properties={"row": 4, "column": 0, "padx": (10, 10), "pady": (5, 5), "sticky": "nsew"},
+            tooltip_text="of_btn_reset_window_settings_tt",
+            loc_func=self.localization_manager.localize
+        )
 
-        self.btn_reset_window_position = ctk.CTkButton(self.window_settings_frame, text=self.loc("btn_reset_window_position"))
-        self.btn_reset_window_position.configure(font=FONT_BIG_BOLD)
-        self.btn_reset_window_position.grid(row=6, column=0, padx=(10, 10), pady=(5, 10), sticky="nsew")
+        self.btn_reset_window_size = CustomCTKButton(
+            btn_text="of_btn_reset_window_size", btn_properties={"master": self.window_settings_frame, "font": FONT_BIG_BOLD},
+            pack_type="grid", pack_properties={"row": 5, "column": 0, "padx": (10, 10), "pady": (5, 5), "sticky": "nsew"},
+            tooltip_text="of_btn_reset_window_size_tt",
+            loc_func=self.localization_manager.localize
+        )
+
+        self.btn_reset_window_pos = CustomCTKButton(
+            btn_text="of_btn_reset_window_pos", btn_properties={"master": self.window_settings_frame, "font": FONT_BIG_BOLD},
+            pack_type="grid", pack_properties={"row": 6, "column": 0, "padx": (10, 10), "pady": (5, 10), "sticky": "nsew"},
+            tooltip_text="of_btn_reset_window_pos_tt",
+            loc_func=self.localization_manager.localize
+        )
 
     def create_ui_appearance_frame(self) -> None:
         """Create the frame and buttons for UI appearance settings."""
-        self.ui_appearance_frame = ctk.CTkFrame(self)
-        self.ui_appearance_frame.grid(row=0, column=1, padx=(5, 10), pady=(10, 5), sticky="nsew")
+        self._configure_grid(self.ui_appearance_frame, row_weights=[(0, 0)], column_weights=[(0, 0)])
 
-        self.ui_appearance_frame.rowconfigure(0, weight=0)
-        self.ui_appearance_frame.columnconfigure(0, weight=0)
-
-        self.label_ui_appearance = ctk.CTkLabel(self.ui_appearance_frame, text=self.loc("label_ui_appearance"))
-        self.label_ui_appearance.configure(font=FONT_BIG_BOLD)
-        self.label_ui_appearance.grid(row=0, column=0, columnspan=2, padx=(10, 10), pady=(10, 5), sticky="nsew")
-
-        self.checkbox_use_high_dpi_scaling = ctk.CTkCheckBox(
-            master=self.ui_appearance_frame,
-            text=self.loc("checkbox_use_high_dpi_scaling"),
-            variable=CH.get_variable_value(CKL.USE_HIGH_DPI_SCALING),
-            onvalue=True,
-            offvalue=False
+        self.lbl_ui_appearance_settings = CustomCTKLabel(
+            label_text="of_lbl_appearance_settings", label_properties={"master": self.ui_appearance_frame, "font": FONT_BIG_BOLD},
+            pack_type="grid", pack_properties={"row": 0, "column": 0, "columnspan": 2, "padx": (10, 10), "pady": (10, 5), "sticky": "nsew"},
+            loc_func=self.localization_manager.localize
         )
-        self.checkbox_use_high_dpi_scaling.grid(row=1, column=0, columnspan=2, padx=(10, 10), pady=(5, 5), sticky="nsew")
-        CustomTooltip(self.checkbox_use_high_dpi_scaling, self.loc("tooltip_use_high_dpi_scaling"))
-
-        self.label_ui_theme = ctk.CTkLabel(self.ui_appearance_frame, text=self.loc("label_ui_theme"))
-        self.label_ui_theme.configure(font=FONT_BIG)
-        self.label_ui_theme.grid(row=2, column=0, padx=(10, 5), pady=(5, 5), sticky="nsew")
-
-        self.dropdown_ui_theme = ctk.CTkOptionMenu(
-            master=self.ui_appearance_frame,
-            variable=CH.get_variable_value(CKL.UI_THEME),
-            values=CH.get_variable_value(CKL.DROPDOWN_UI_THEMES),
-            font=FONT_BIG_BOLD
+        self.checkbox_use_high_dpi_scaling = CustomCTKCheckbox(
+            checkbox_text="of_checkbox_use_high_dpi_scaling",
+            checkbox_properties={"master": self.ui_appearance_frame, "variable": CH.get_variable_value(CKL.USE_HIGH_DPI_SCALING), "onvalue": True, "offvalue": False, "font": FONT_BIG},
+            pack_type="grid", pack_properties={"row": 1, "column": 0, "columnspan": 2, "padx": (10, 10), "pady": (5, 5), "sticky": "nsew"},
+            tooltip_text="of_checkbox_use_high_dpi_scaling_tt",
+            loc_func=self.localization_manager.localize
         )
-        self.dropdown_ui_theme.grid(row=2, column=1, padx=(5, 10), pady=(5, 5), sticky="nsew")
-        CustomTooltip(self.dropdown_ui_theme, self.loc("tooltip_ui_theme"))
 
-        self.label_ui_color_theme = ctk.CTkLabel(self.ui_appearance_frame, text=self.loc("label_ui_color_theme"))
-        self.label_ui_color_theme.configure(font=FONT_BIG)
-        self.label_ui_color_theme.grid(row=3, column=0, padx=(10, 5), pady=(5, 5), sticky="nsew")
-
-        self.dropdown_ui_color_theme = ctk.CTkOptionMenu(
-            master=self.ui_appearance_frame,
-            variable=CH.get_variable_value(CKL.UI_COLOR_THEME),
-            values=CH.get_variable_value(CKL.DROPDOWN_UI_COLOR_THEMES),
-            font=FONT_BIG_BOLD
+        self.lbl_ui_theme = CustomCTKLabel(
+            label_text="of_lbl_ui_theme", label_properties={"master": self.ui_appearance_frame, "font": FONT_BIG_BOLD},
+            pack_type="grid", pack_properties={"row": 2, "column": 0, "padx": (10, 5), "pady": (5, 5), "sticky": "nsew"},
+            loc_func=self.localization_manager.localize
         )
-        self.dropdown_ui_color_theme.grid(row=3, column=1, padx=(5, 10), pady=(5, 5), sticky="nsew")
-        CustomTooltip(self.dropdown_ui_color_theme, self.loc("tooltip_ui_color_theme"))
-
-        self.label_ui_language = ctk.CTkLabel(self.ui_appearance_frame, text=self.loc("label_ui_language"))
-        self.label_ui_language.configure(font=FONT_BIG)
-        self.label_ui_language.grid(row=4, column=0, padx=(10, 5), pady=(5, 5), sticky="nsew")
-
-        self.dropdown_ui_language = ctk.CTkOptionMenu(
-            master=self.ui_appearance_frame,
-            variable=CH.get_variable_value(CKL.UI_LANGUAGE),
-            values=CH.get_variable_value(CKL.DROPDOWN_UI_LANGUAGES),
-            font=FONT_BIG_BOLD
+        self.dropdown_ui_theme = CustomCTKOptionMenu(
+            placeholder_text="of_dropdown_ui_theme_placeholder", options=CH.get_variable_value(CKL.DROPDOWN_UI_THEMES),
+            optionmenu_properties={"master": self.ui_appearance_frame, "font": FONT_BIG_BOLD},
+            pack_type="grid", pack_properties={"row": 2, "column": 1, "padx": (5, 10), "pady": (5, 5), "sticky": "nsew"},
+            tooltip_text="of_dropdown_ui_theme_tt",
+            loc_func=self.localization_manager.localize
         )
-        self.dropdown_ui_language.grid(row=4, column=1, padx=(5, 10), pady=(5, 5), sticky="nsew")
-        CustomTooltip(self.dropdown_ui_language, self.loc("tooltip_ui_language"))
 
-        self.btn_reset_ui_appearance_settings = ctk.CTkButton(self.ui_appearance_frame, text=self.loc("btn_reset_ui_appearance_settings"))
-        self.btn_reset_ui_appearance_settings.configure(font=FONT_BIG_BOLD)
-        self.btn_reset_ui_appearance_settings.grid(row=5, column=0, columnspan=2, padx=(10, 10), pady=(5, 10), sticky="nsew")
+        self.lbl_ui_color_theme = CustomCTKLabel(
+            label_text="of_lbl_ui_color_theme", label_properties={"master": self.ui_appearance_frame, "font": FONT_BIG_BOLD},
+            pack_type="grid", pack_properties={"row": 3, "column": 0, "padx": (10, 5), "pady": (5, 5), "sticky": "nsew"},
+            loc_func=self.localization_manager.localize
+        )
+        self.dropdown_ui_color_theme = CustomCTKOptionMenu(
+            placeholder_text="of_dropdown_ui_color_theme_placeholder", options=CH.get_variable_value(CKL.DROPDOWN_UI_COLOR_THEMES),
+            optionmenu_properties={"master": self.ui_appearance_frame, "font": FONT_BIG_BOLD},
+            pack_type="grid", pack_properties={"row": 3, "column": 1, "padx": (5, 10), "pady": (5, 5), "sticky": "nsew"},
+            tooltip_text="of_dropdown_ui_color_theme_tt",
+            loc_func=self.localization_manager.localize
+        )
+
+        self.lbl_ui_language = CustomCTKLabel(
+            label_text="of_lbl_ui_language", label_properties={"master": self.ui_appearance_frame, "font": FONT_BIG_BOLD},
+            pack_type="grid", pack_properties={"row": 4, "column": 0, "padx": (10, 5), "pady": (5, 5), "sticky": "nsew"},
+            loc_func=self.localization_manager.localize
+        )
+        self.dropdown_ui_language = CustomCTKOptionMenu(
+            placeholder_text="of_dropdown_ui_language_placeholder", options=CH.get_variable_value(CKL.DROPDOWN_UI_LANGUAGES),
+            optionmenu_properties={"master": self.ui_appearance_frame, "font": FONT_BIG_BOLD},
+            pack_type="grid", pack_properties={"row": 4, "column": 1, "padx": (5, 10), "pady": (5, 5), "sticky": "nsew"},
+            tooltip_text="of_dropdown_ui_language_tt",
+            loc_func=self.localization_manager.localize
+        )
+
+        self.btn_reset_ui_appearance_settings = CustomCTKButton(
+            btn_text="of_btn_reset_ui_appearance_settings", btn_properties={"master": self.ui_appearance_frame, "font": FONT_BIG_BOLD},
+            pack_type="grid", pack_properties={"row": 5, "column": 0, "columnspan": 2, "padx": (10, 10), "pady": (5, 10), "sticky": "nsew"},
+            tooltip_text="of_btn_reset_ui_appearance_settings_tt",
+            loc_func=self.localization_manager.localize
+        )
 
     def create_translation_settings_frame(self) -> None:
         """Create the frame and buttons for translation settings."""
-        self.translation_settings_frame = ctk.CTkFrame(self)
-        self.translation_settings_frame.grid(row=1, column=0, padx=(10, 5), pady=(5, 10), sticky="nsew")
 
-        self.translation_settings_frame.rowconfigure(0, weight=0)
-        self.translation_settings_frame.columnconfigure(0, weight=0)
+        self._configure_grid(self.translation_settings_frame, row_weights=[(0, 0)], column_weights=[(0, 0)])
 
-        self.label_translation_settings = ctk.CTkLabel(self.translation_settings_frame, text=self.loc("label_translation_settings"))
-        self.label_translation_settings.configure(font=FONT_BIG_BOLD)
-        self.label_translation_settings.grid(row=0, column=0, padx=(10, 10), pady=(10, 5), sticky="nsew")
-
-        self.checkbox_whole_word_replacement = ctk.CTkCheckBox(
-            master=self.translation_settings_frame,
-            text=self.loc("checkbox_whole_word_replacement"),
-            variable=CH.get_variable_value(CKL.WHOLE_WORD_REPLACEMENT),
-            onvalue=True,
-            offvalue=False
+        self.lbl_translation_settings = CustomCTKLabel(
+            label_text="of_lbl_translation_settings", label_properties={"master": self.translation_settings_frame, "font": FONT_BIG_BOLD},
+            pack_type="grid", pack_properties={"row": 0, "column": 0, "padx": (10, 10), "pady": (10, 5), "sticky": "nsew"},
+            loc_func=self.localization_manager.localize
         )
-        self.checkbox_whole_word_replacement.grid(row=1, column=0, padx=(10, 10), pady=(5, 10), sticky="nsew")
-        CustomTooltip(self.checkbox_whole_word_replacement, self.loc("tooltip_whole_word_replacement"))
+
+        self.checkbox_whole_word_replacement = CustomCTKCheckbox(
+            checkbox_text="of_checkbox_whole_word_replacement",
+            checkbox_properties={"master": self.translation_settings_frame, "variable": CH.get_variable_value(CKL.WHOLE_WORD_REPLACEMENT), "onvalue": True, "offvalue": False, "font": FONT_BIG},
+            pack_type="grid", pack_properties={"row": 1, "column": 0, "padx": (10, 10), "pady": (5, 10), "sticky": "nsew"},
+            tooltip_text="of_checkbox_whole_word_replacement_tt",
+            loc_func=self.localization_manager.localize
+        )
+
+    def _construct_frame(self, parent, **grid_options):
+        """Create and grid a CTkFrame within the given parent."""
+        frame = ctk.CTkFrame(parent)
+        frame.grid(**grid_options)
+        return frame
+
+    def _configure_grid(self, frame: ctk.CTkFrame, row_weights: Optional[list[tuple[int, int]]] = None, column_weights: Optional[list[tuple[int, int]]] = None) -> None:
+        """Configure grid weights for rows and columns in the given frame."""
+        if row_weights:
+            for row, weight in row_weights:
+                frame.rowconfigure(row, weight=weight)
+        if column_weights:
+            for column, weight in column_weights:
+                frame.columnconfigure(column, weight=weight)
